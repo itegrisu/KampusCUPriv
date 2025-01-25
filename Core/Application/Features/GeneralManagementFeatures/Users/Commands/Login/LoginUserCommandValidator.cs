67adb2a65fcff258851d25f4e51854d0f9da8aec
Ahmet Mücahit Şimşek <itegrisu@gmail.com>
2025-01-25 23:46:14 +0300

    login apileri yazildi

---
+++ Core/Application/Features/GeneralManagementFeatures/Users/Commands/Login/LoginUserCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.GeneralManagementFeatures.Users.Commands.Login
+{
+    public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
+    {
+        public LoginUserCommandValidator()
+        {
+            RuleFor(c => c.Email).NotNull().NotEmpty();
+            RuleFor(c => c.Password).NotNull().NotEmpty();
+        }
+    }
+}
