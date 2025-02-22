3c314424169f7c3cd93af0478a09a26fae60bfe4
Ahmet Mücahit Şimşek <itegrisu@gmail.com>
2025-02-23 02:04:13 +0300

    Firebase eklendi

---
+++ Infrastracture/Persistence/Migrations/20250222222023_mig5.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class mig5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeviceToken",
+                table: "Users",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeviceToken",
+                table: "Users");
+        }
+    }
+}
