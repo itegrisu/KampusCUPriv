14d394cb358aaeb676ebcdd0357fe0f446d64a43
Ahmet Mücahit Şimşek <itegrisu@gmail.com>
2025-03-06 19:54:33 +0300

    Clubte başkanın soyismi de dönüyor

--- Core/Application/Features/ClubManagementFeatures/Clubs/Queries/GetList/GetListClubListItemDto.cs
+++ Core/Application/Features/ClubManagementFeatures/Clubs/Queries/GetList/GetListClubListItemDto.cs
@@ -8,6 +8,7 @@ public class GetListClubListItemDto : IDto
     public Guid Gid { get; set; }
     public Guid? GidManagerFK { get; set; }
     public string UserFKName { get; set; }
+    public string UserFKLastName { get; set; }
     public Guid GidCategoryFK { get; set; }
     public string CategoryFKName { get; set; }
     public string Name { get; set; }
